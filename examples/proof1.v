ac651be79cb827831b48c62ec27bd235ef38d39a
Martim Afonso <martimafonso185@gmail.com>
2025-12-12 12:59:15 +0100

    added second proof

--- examples/proof1.v
+++ examples/proof1.v
@@ -5,3 +5,11 @@ Proof.
   - reflexivity.
   - simpl. rewrite IHl. simpl. reflexivity.
 Qed.
+
+Lemma assoc_rev A :
+  forall (l1 l2 : list A), rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  induction l1.
+  - simpl. rewrite rev_nil. reflexivity.
+  - simpl. rewrite IHl1. simpl. rewrite app_assoc. reflexivity.
+Qed.
